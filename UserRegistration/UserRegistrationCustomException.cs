c6774a7897f60dac326e2c3feff3825503882095
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-12 01:26:53 +0530

    [SUNNY] Add. UseCase12 for handling custom exceptions

---
+++ UserRegistration/UserRegistrationCustomException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace UserRegistration
+{
+    public class UserRegistrationCustomException:Exception
+    {
+        ///<param name="message"></param>
+        public UserRegistrationCustomException(string message) : base(message)
+        {
+           
+        }
+    }
+}
